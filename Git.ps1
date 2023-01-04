631c82285df6b1e4513c7c0a03034a8de63c8710
karlronsaria <andrew.mikedaniels@gmail.com>
2023-01-04 14:23:25 -0800

    first commit

---
+++ Git.ps1
@@ -0,0 +1,36 @@
+function Start-GitPullRequest {
+    Param(
+        [String]
+        $Directory,
+
+        [String]
+        $Remote = 'origin',
+
+        [String]
+        $Branch = 'master'
+    )
+
+    Set-Location $Directory
+    Invoke-Expression "git pull"
+}
+
+function Start-ScriptModuleGitPullRequest {
+    Param(
+        [String]
+        $Directory
+    )
+
+    $what = Join-Path $Directory 'repo.json' `
+        | Get-Item `
+        | Get-Content `
+        | ConvertFrom-Json
+
+    foreach ($repository in $what.Repository) {
+        $path = Join-Path $Directory $repository
+
+        Start-GitPullRequest `
+            -Directory $path `
+            -Remote $what.DefaultRemote `
+            -Branch $what.DefaultBranch
+    }
+}
---
+++ Profile.ps1
@@ -0,0 +1,86 @@
+<#
+.LINK
+- Url: https://serverfault.com/questions/95431/in-a-powershell-script-how-can-i-check-if-im-running-with-administrator-privil
+- Retrieved: 2023_01_04
+#>
+function Test-RoleIsAministrator {
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+    $principal = New-Object Security.Principal.WindowsPrincipal($identity)
+    return $principal.IsInRole($adminRole)
+}
+
+function Get-ProfileLocation {
+    return Split-Path $PROFILE -Parent
+}
+
+# 2022_02_23
+function Get-ConsoleHostHistory {
+    Param(
+        [Switch]
+        $FilePath
+    )
+
+    $path = "$($env:APPDATA)\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+
+    if ($FilePath) {
+        return $path
+    }
+
+    cat $path
+}
+
+function Run-MyCommand {
+    Param(
+        [Parameter(ValueFromPipeline = $true)]
+        [String]
+        $Command,
+
+        [Object[]]
+        $ArgumentList
+    )
+
+    Invoke-Command `
+        -ScriptBlock ([ScriptBlock]::Create($Command.Trim('"'))) `
+        -ArgumentList:$ArgumentList
+}
+
+function Get-ScriptModuleSourceCommand {
+    Param(
+        [String]
+        $InfoDir
+    )
+
+    if (-not $InfoDir) {
+        $InfoDir = $PsScriptRoot
+    }
+
+    $command = @"
+`$repo = dir '$InfoDir\repo.json' | cat | ConvertFrom-Json;
+
+foreach (`$module in `$repo.ScriptModule) {
+    iex "$InfoDir\`$module\Get-Scripts.ps1" | % { . `$_ }
+};
+
+if ((Test-RoleIsAministrator)) {
+    foreach (`$module in `$repo.ElevatedScriptModule) {
+        iex "$InfoDir\`$module\Get-Scripts.ps1" | % { . `$_ }
+    }
+};
+"@
+
+    return $command
+}
+
+New-Alias `
+    -Name 'Pull-ScriptModule' `
+    -Value 'Start-ScriptModuleGitPullRequest'
+
+
+
+
+
+
+
+
+
