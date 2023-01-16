caad4dfedd7eb8a8046bf2e91a70d6e953149e10
karlronsaria <andrew.mikedaniels@gmail.com>
2023-01-16 14:43:39 -0800

    feat: add sample profile

---
+++ sample/profile.ps1
@@ -0,0 +1,19 @@
+. "$PsScriptRoot\Scripts\MyPsShortcut\MyPsShortcut.ps1"
+. "$PsScriptRoot\Scripts\ActionItem.ps1"
+. "$PsScriptRoot\Scripts\PsalmOfTheDay.ps1"
+. "$PsScriptRoot\Scripts\Frivolous.ps1" # Imports Posh-Git module
+
+# First script module
+& "$PsScriptRoot\Scripts\PsProfile\Get-Scripts.ps1" | % { . $_ }
+
+# Store previous command's output in `$__`
+$PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
+
+# link
+# - url: https://stackoverflow.com/questions/40098771/changing-powershells-default-output-encoding-to-utf-8
+# - retrieved: 2023_01_16
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
+Invoke-Expression (Get-ScriptModuleSourceCommand)
+
+Set-PsReadLineOption -EditMode Vi
