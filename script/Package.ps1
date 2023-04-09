d752b0eecdc43a543c7eea9f6c1b1b7b991a0af5
karlronsaria <andrew.mikedaniels@gmail.com>
2023-04-09 16:23:27 -0700

    feat: Package.ps1

---
+++ script/Package.ps1
@@ -0,0 +1,30 @@
+﻿function New-PackageJson {
+    Param(
+        $Path,
+
+        [Switch]
+        $PassThru
+    )
+
+    if (-not $Path) {
+        $Path = "$PsScriptRoot/../res"
+    }
+
+    $list = Get-InstalledModule `
+        | select Name, Version, Repository `
+        | foreach {
+            [PsCustomObject]@{
+                Name = $_.Name
+                Version = $_.Version.ToString()
+                Repository = $_.Repository
+            }
+        }
+
+    if ($PassThru) {
+        Write-Output $list
+    }
+
+    $list `
+        | ConvertTo-Json `
+        | Out-File (Join-Path $Path "package.json")
+}
