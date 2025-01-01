5cb9a93f11a90f4ac54ea5e377e348664c47cff6
karlr <andrew.mikedaniels@gmail.com>
2025-01-01 03:59:54 -0800

    chore: update sample

--- sample/profile.ps1
+++ sample/profile.ps1
@@ -1,3 +1,19 @@
+# function Import-ModuleWithNotification {
+#     param(
+#         [Parameter(Mandatory=$true, Position=0)]
+#         [string]$Name
+#     )
+#     Write-Host "Importing module: $Name"
+#     Import-Module $Name -Verbose
+# }
+# 
+# # Override the default Import-Module with the verbose notification version
+# Remove-Item alias:Import-Module -Force
+# Set-Alias -Name Import-Module -Value Import-ModuleWithNotification
+
+
+Import-Module Posh-Git
+
 $loc = "$($env:OneDrive)\Documents\WindowsPowerShell"
 
 $myScripts = @(
@@ -18,15 +34,19 @@ $myScriptModules | foreach {
     iex $_ | foreach { . $_ }
 }
 
-Start-ModuleImportProgressRunner
+# Start-ModuleImportProgressRunner
 
-"$loc\Modules" | dir | foreach { $_.Name } | foreach {
-    Import-Module $_ -ErrorAction SilentlyContinue
-}
+# "$loc\Modules" | dir | foreach { $_.Name } | foreach {
+#     Import-Module $_ -ErrorAction SilentlyContinue
+# }
 
 New-Alias `
     -Name 'gchrome' `
     -Value 'Run-ShortcutGoogleChromeProfile'
+
+New-Alias `
+    -Name 'gchromepanic' `
+    -Value 'Stop-ShortcutGoogleChrome'
 
 # Store previous command's output in `$__`
 $PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
@@ -36,7 +56,13 @@ $PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
 # - retrieved: 2023_01_16
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 
+# (karlr 2024_09_22)
+# link
+# - url: <https://starship.rs/>
+# - retrieved: 2024_09_22
+Invoke-Expression (&starship init powershell)
+
 Invoke-Expression (Get-ScriptModuleSourceCommand -ShowProgress)
-Stop-ModuleImportProgressRunner
+# Stop-ModuleImportProgressRunner
 Set-PsReadLineOption -EditMode Vi
 
